eb6c7302ff1fdc5de819b90cb913d1f64295a1f1
Dastan Tashimbetov <dastan@Dastans-MacBook-Pro.local>
2017-12-06 00:13:34 -0800

    initial commit

---
+++ Fair/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Fair
+//
+//  Created by Dastan Tashimbetov on 12/5/17.
+//  Copyright © 2017 Dastan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
