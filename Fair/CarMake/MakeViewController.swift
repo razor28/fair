fc1029d4d56b1ee4bf5e91fa46273c4cb35dbee6
Dastan Tashimbetov <arabesku@gmail.com>
2017-12-07 00:17:26 -0800

    Add table view and search bar. Fix cell size

--- Fair/CarMake/MakeViewController.swift
+++ Fair/CarMake/MakeViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 final class MakeViewController: UIViewController, Reloadable {
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var tableView: UITableView!
+
     var carDataSource: CarDataSource?
+
+    private let cellIdentifier = String(describing: MakeCell.self)
+    private var items = [Make]()
 
     static func instantiateFromStoryboard() -> MakeViewController? {
         let storyboard = UIStoryboard(name: "Make", bundle: nil)
@@ -20,5 +26,28 @@ final class MakeViewController: UIViewController, Reloadable {
 
     func reloadData() {
 
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableView()
+    }
+
+    private func setupTableView() {
+        let cellNib = UINib(nibName: cellIdentifier, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: cellIdentifier)
+    }
+}
+
+extension MakeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as? MakeCell else { return UITableViewCell() }
+//        let make = items[indexPath.row]
+//        cell.makeLabel.text = make.niceName
+        return cell
     }
 }
--- FairDevTests/CarMake/MakeViewControllerTests.swift
+++ FairDevTests/CarMake/MakeViewControllerTests.swift
@@ -10,7 +10,25 @@ import XCTest
 @testable import FairDev
 
 class MakeViewControllerTests: XCTestCase {
-    func testCanBeInitializedFromStoryboard() {
-        XCTAssertNotNil(MakeViewController.instantiateFromStoryboard())
+    var makeViewController: MakeViewController!
+
+    override func setUp() {
+        super.setUp()
+        guard let viewController = MakeViewController.instantiateFromStoryboard() else {
+            XCTFail("Can't instanciate makeViewController from storyboard")
+            return
+        }
+        _ = viewController.view
+        makeViewController = viewController
+    }
+
+    func testCanBeInitializedFromStoryboard() { }
+
+    func testHasTableView() {
+        XCTAssertNotNil(makeViewController.tableView)
+    }
+
+    func testHasSearchBar() {
+        XCTAssertNotNil(makeViewController.searchBar)
     }
 }
