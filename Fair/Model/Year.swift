5a92fd48babb91ee968b3d33622a506308d5099b
Dastan Tashimbetov <arabesku@gmail.com>
2017-12-06 21:43:09 -0800

    Add car make, model and year models

---
+++ Fair/Model/Year.swift
@@ -0,0 +1,14 @@
+//
+//  Year.swift
+//  Fair
+//
+//  Created by Dastan Tashimbetov on 12/6/17.
+//  Copyright © 2017 Dastan. All rights reserved.
+//
+
+import Foundation
+
+struct Year {
+    let id: Int
+    let year: Int
+}
